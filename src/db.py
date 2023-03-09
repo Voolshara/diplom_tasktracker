230dbbb454a53db30336c51161afae21ac949a36
StanisLove <sta-tun-slav@yandex.ru>
2023-03-10 01:42:52 +0300

    finish project

--- src/db.py
+++ src/db.py
@@ -38,9 +38,9 @@ def create_session(**kwargs):
 
 
 class Task(Base):
-    __tablename__ = 'City'
+    __tablename__ = 'Task'
     id = sa.Column(sa.Integer, primary_key=True)
-    taskName = sa.Column(sa.String())
+    taskName = sa.Column(sa.String(), unique=True)
     phone = sa.Column(sa.String())
     email = sa.Column(sa.String())
     nickName = sa.Column(sa.String())
@@ -49,10 +49,73 @@ class Task(Base):
     phone_text = sa.Column(sa.String())
     status = sa.Column(sa.Boolean())
 
-
+def check_task(task_name: str) -> bool:
+    with create_session() as session:
+        db_response = session.query(Task).filter(Task.taskName == task_name).one_or_none()
+        return db_response is None
+    
+def get_all_running_tasks() -> list:
+    with create_session() as session:
+        db_response = session.query(Task).filter(Task.status == True).all()
+        if len(db_response) == 0:
+            return []
+        out_data = [[
+            el.id, el.taskName, el.phone, el.email, el.nickName, el.name, el.addInfo, el.phone_text, el.status
+            ] for el in db_response]
+        out_data.sort(key=lambda x: x[0])
+        return out_data
+    
+def get_all_close_tasks() -> list:
+    with create_session() as session:
+        db_response = session.query(Task).filter(Task.status == False).all()
+        if len(db_response) == 0:
+            return []
+        out_data = [[
+            el.id, el.taskName, el.phone, el.email, el.nickName, el.name, el.addInfo, el.phone_text, el.status
+            ] for el in db_response]
+        out_data.sort(key=lambda x: x[0])
+        return out_data
+    
 def new_task(task_name: str) -> str:
-    pass
+    with create_session() as session:
+        session.add(
+            Task(
+                taskName = task_name,
+                phone = "",
+                email = "",
+                nickName = "",
+                name = "",
+                addInfo = "",
+                phone_text = "",
+                status = True,
+            )
+        )
 
+def get_now_phone(task_id: int) -> list:
+    with create_session() as session:
+        db_response = session.query(Task).filter(Task.id == task_id).one_or_none()
+        return [db_response.phone, db_response.phone_text] if db_response is not None else ["", ""]
+
+def set_data(form_data: dict) -> None:
+    with create_session() as session:
+        session.query(Task).filter(
+            Task.id == int(form_data['task'])
+            ).update({
+                Task.phone: form_data['phone'],
+                Task.email: form_data['email'],
+                Task.nickName: form_data['Nickname'],
+                Task.name: form_data['name'],
+                Task.addInfo: form_data['addinfo'],
+                Task.phone_text: form_data['phone_text'],
+            })
+        
+def change_status(task_id: int) -> None:
+    with create_session() as session:
+        session.query(Task).filter(
+            Task.id == task_id
+            ).update({
+                Task.status: False
+            })
 
 def create_all_tables():
     Base.metadata.create_all(engine)
--- src/main.py
+++ src/main.py
@@ -1,35 +1,53 @@
-from flask import Flask, render_template, request, redirect, url_for
+from src.db import check_task, new_task, get_all_running_tasks, get_now_phone, set_data, change_status, get_all_close_tasks
 
+from phonenumbers import parse as phone_parse
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-tasks = []
-completed_tasks = []
+# tasks = []
+# completed_tasks = []
 
 @app.route('/')
 def index():
+    args = request.args.to_dict()
+    tasks = get_all_running_tasks()
+    if "is_task_exist" in args:
+        return render_template('index.html', tasks=tasks, is_task_exist=True)
     return render_template('index.html', tasks=tasks)
 
 @app.route('/new', methods=['POST'])
 def add_task():
     task = request.form['task']
-    tasks.append(task)
-    return redirect(url_for('index'))
-
+    if check_task(task):
+        new_task(task)
+        return redirect(url_for('index'))
+    return redirect(url_for('index', is_task_exist=True))
+    
 @app.route('/add_info', methods=['POST'])
 def add_task_info():
-    print(request.form)
+    # ImmutableMultiDict([('phone', ''), ('email', 'sdf'), ('Nickname', ''), ('name', ''), ('addinfo', ''), ('task', 'dfs')])
+    task_data = request.form.to_dict()
+    old_phone, old_phone_text = get_now_phone(int(task_data['task']))
+    if old_phone == task_data['phone']:
+        task_data['phone_text'] = old_phone_text
+    else:
+        try:
+            phone_info = str(phone_parse(task_data['phone']))
+        except:
+            phone_info = ""
+        task_data['phone_text'] = phone_info
+    set_data(task_data)
     return redirect(url_for('index'))
 
 @app.route('/complete/<int:task_id>')
 def complete_task(task_id):
-    completed_task = tasks.pop(task_id)
-    completed_tasks.append(completed_task)
-    print(tasks, completed_tasks)
+    change_status(int(task_id))
     return redirect(url_for('index'))
 
 @app.route('/archive')
 def archive():
+    completed_tasks = get_all_close_tasks()
     return render_template('archive.html', completed_tasks=completed_tasks)
 
 def run():
